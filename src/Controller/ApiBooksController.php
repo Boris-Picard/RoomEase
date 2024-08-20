733d90a969f4aaf03111d83dae0275d54726ee7f
boris-picard <picard.boris@gmail.com>
2024-08-20 12:23:45 +0200

    feat(BooksRepository - ApiBooksController): added find method & route prefix & methods

--- src/Controller/ApiBooksController.php
+++ src/Controller/ApiBooksController.php
@@ -7,13 +7,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/books')]
 class ApiBooksController extends AbstractController
 {
-    #[Route('api/books')]
+    #[Route('', methods: ['GET'])]
 
     public function getBooks(BooksRepository $booksRepository): Response
     {
         $books = $booksRepository->getAllBooks();
         return $this->json($books);
+    }
+
+    // #[Route('', methods: ['POST'])]
+    // public function create() {}
+
+    #[Route('/{id<d+>}', methods: ['GET'])]
+    public function get(int $id, BooksRepository $booksRepository): Response
+    {
+        $book = $booksRepository->find($id);
+        
+        return $this->json($book);
     }
 }
--- src/Repository/BooksRepository.php
+++ src/Repository/BooksRepository.php
@@ -10,4 +10,14 @@ class BooksRepository
     {
         return [new Books(1, '1984', 'George Orwell', 1949, 'Dystopian'), new Books(2, 'To Kill a Mockingbird', 'Harper Lee', 1960, 'Fiction'), new Books(3, 'The Great Gatsby', 'F. Scott Fitzgerald', 1925, 'Classic')];
     }
+
+    public function find(int $id): ?Books
+    {
+        foreach ($this->getAllBooks() as $book) {
+            if ($book->id === $id) {
+                return $book;
+            }
+        }
+        return null;
+    }
 }
