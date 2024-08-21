a1101e64e204b093f67ede43038754524e6eb60e
boris-picard <picard.boris@gmail.com>
2024-08-21 22:36:45 +0200

    feat(AccountController): added a condition to verify if user already exist

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Account;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,6 +14,14 @@ class AccountController extends AbstractController
     #[Route('/account', name: 'create_account')]
     public function createAccount(EntityManagerInterface $entityManager): Response
     {
+        $existingAccount = $entityManager->getRepository(Account::class)
+            ->findOneBy(['email' => 'boris2@gmail.com']);
+
+        // Si un utilisateur existe déjà, lancer une exception ou retourner une réponse appropriée
+        if ($existingAccount) {
+            throw new Exception('L\'utilisateur existe déjà avec cet email');
+        }
+
         $account = new Account();
         $account->setEmail('boris2@gmail.com');
         $account->setPassword('1234');
