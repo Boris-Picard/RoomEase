a42b1f8f594be9d239e0cd83dc198de53d4007d2
boris-picard <picard.boris@gmail.com>
2024-08-26 15:56:20 +0200

    feat: added my template for form

--- src/Form/RoomType.php
+++ src/Form/RoomType.php
@@ -4,16 +4,30 @@ namespace App\Form;
 
 use App\Entity\Room;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RoomType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('capacity')
+            ->add('name', TextType::class, [
+                "label" => 'Entrez un nom valide',
+                "constraints" => [
+                    new NotBlank(),
+                    new Length(["min" => 2]),
+                ]
+            ])
+            ->add('capacity', null, [
+                "constraints" => [
+                    new NotBlank(),
+                    new Length(["min" => 2]),
+                ]
+            ])
             ->add('equipment')
         ;
     }
