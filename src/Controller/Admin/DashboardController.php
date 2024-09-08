aafb61127b2f0eac075d0734e7ffbd19da5809ac
boris-picard <picard.boris@gmail.com>
2024-09-08 14:53:14 +0200

    feat(DashboardController): manage to redirect admin to first crud reservation with /admin page

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,6 +8,7 @@ use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -22,8 +23,8 @@ class DashboardController extends AbstractDashboardController
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(ReservationCrudController::class)->generateUrl());
 
         // Option 2. You can make your dashboard redirect to different pages depending on the user
         //
@@ -34,7 +35,7 @@ class DashboardController extends AbstractDashboardController
         // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
         // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
         //
-        return $this->render('/admin/index.html.twig');
+        // return $this->render('/admin/index.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -46,7 +47,7 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToCrud('Reservations', 'fa fa-bookmark', Reservation::class);
-        yield MenuItem::linkToCrud('Room', 'fa fa-bed', Room::class);
-        yield MenuItem::linkToCrud('Reservations', 'fa fa-user', User::class);
+        yield MenuItem::linkToCrud('Rooms', 'fa fa-bed', Room::class);
+        yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class);
     }
 }
